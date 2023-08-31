6a6a4d8c02426162d6c99344f7913ea6aa9d37c8
Sarthak Vaish <sarthak@simplifii.com>
2023-08-31 14:52:33 +0530

    added error message

--- src/ListView.tsx
+++ src/ListView.tsx
@@ -2,27 +2,40 @@ import List from "@mui/material/List";
 import SingleDetails from "./SingleDetails";
 import { colors } from "./constants";
 import { Cluster, Clusters } from "./types";
+import { Typography } from "@mui/material";
 
 type Props = {
-  data: Clusters | null,
-  setActiveCluster: (data: Cluster | null) => void
-}
+  data: Clusters | null;
+  setActiveCluster: (data: Cluster | null) => void;
+};
 
-export default function ListView({data, setActiveCluster}: Props) {
+export default function ListView({ data, setActiveCluster }: Props) {
   return (
-    <List sx={{ height: "100%", overflowY:"scroll" , bgcolor: "background.paper", py: 0 }}>
-      {data && data.map((item, index) => (
-        <SingleDetails
-          color={colors[index]}
-          label={item.address}
-          startTime={item?.start_time}
-          endTime={item?.end_time}
-          id={index + 1}
-          routes = {item.route_to_next_cluster}
-          setActiveCluster={setActiveCluster}
-          cluster = {item}
-        />
-      ))}
+    <List
+      sx={{
+        height: "100%",
+        overflowY: "scroll",
+        bgcolor: "background.paper",
+        py: 0,
+      }}
+    >
+      {data &&
+        data.map((item, index) => (
+          <SingleDetails
+            color={colors[index]}
+            label={item.address}
+            startTime={item?.start_time}
+            endTime={item?.end_time}
+            id={index + 1}
+            routes={item.route_to_next_cluster}
+            setActiveCluster={setActiveCluster}
+            cluster={item}
+          />
+        ))}
+
+      {data && data.length === 0 ? (
+        <Typography className="no-location">No locations tracked on this date</Typography>
+      ) : null}
     </List>
   );
 }
